df617f489244f278d35a94182e9036a0f279b2b9
hederdavid <hedermd6@gmail.com>
2025-06-27 10:40:28 -0300

    feat: clients dto

--- src/clients/dto/create-client.dto.ts
+++ src/clients/dto/create-client.dto.ts
@@ -1,3 +1,46 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+  Matches,
+} from 'class-validator';
+
 export class CreateClientDto {
-    
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 100)
+  readonly nome_completo: string;
+
+  @IsEmail({}, { message: 'Email inválido' })
+  readonly email: string;
+
+  @IsString()
+  @Matches(/^\d{5}-\d{3}$/, { message: 'CEP deve estar no formato 00000-000' })
+  readonly cep: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly logradouro: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly cidade: string;
+
+  @IsString()
+  @Matches(/^[A-Z]{2}$/, { message: 'UF deve conter 2 letras maiúsculas' })
+  readonly uf: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly bairro: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly numero: string;
+
+  @IsString()
+  @IsOptional()
+  readonly complemento?: string;
 }
