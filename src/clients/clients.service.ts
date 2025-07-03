7b85d475fa17e8d9be9f314cf4f2b3e9cf61261b
hederdavid <hedermd6@gmail.com>
2025-07-03 16:45:03 -0300

    feat: function to improve the create method

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -6,68 +6,51 @@ import { ClientResponseDto } from './dto/resp-client.dto';
 
 @Injectable()
 export class ClientsService {
-  constructor(
-    private readonly prisma: PrismaService,
-  ) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   async create(createClientDto: CreateClientDto): Promise<ClientResponseDto> {
-  try {
-    const cliente = await this.prisma.cliente.create({
-      data: {
-        nome_completo: createClientDto.nome_completo,
-        email: createClientDto.email,
-        cep: createClientDto.cep,
-        logradouro: createClientDto.logradouro,
-        cidade: createClientDto.cidade,
-        uf: createClientDto.uf,
-        bairro: createClientDto.bairro,
-        numero: createClientDto.numero,
-        complemento: createClientDto.complemento,
-        telefones: {
-          createMany: {
-            data: createClientDto.telefones.map((phone) => ({
-              numero: phone,
-            })),
-          },
-        },
-      },
-      select: {
-        id: true,
-        nome_completo: true,
-        email: true,
-        cep: true,
-        logradouro: true,
-        cidade: true,
-        uf: true,
-        bairro: true,
-        numero: true,
-        complemento: true,
-        telefones: {
-          select: {
-            numero: true,
-          },
-        },
-      },
-    });
 
-    return {
-      id: cliente.id,
-      nome_completo: cliente.nome_completo,
-      email: cliente.email,
-      cep: cliente.cep,
-      logradouro: cliente.logradouro,
-      cidade: cliente.cidade,
-      uf: cliente.uf,
-      bairro: cliente.bairro,
-      numero: cliente.numero,
-      complemento: cliente.complemento ?? undefined,
-      telefones: cliente.telefones.map((t) => t.numero),
-    };
-  } catch (error) {
-    throw new Error(`Erro ao criar cliente: ${error.message}`);
+    const data = this._mapCreateClientDtoToPrismaData(createClientDto);
+    
+    try {
+      const cliente = await this.prisma.cliente.create({
+        data,
+        select: {
+          id: true,
+          nome_completo: true,
+          email: true,
+          cep: true,
+          logradouro: true,
+          cidade: true,
+          uf: true,
+          bairro: true,
+          numero: true,
+          complemento: true,
+          telefones: {
+            select: {
+              numero: true,
+            },
+          },
+        },
+      });
+
+      return {
+        id: cliente.id,
+        nome_completo: cliente.nome_completo,
+        email: cliente.email,
+        cep: cliente.cep,
+        logradouro: cliente.logradouro,
+        cidade: cliente.cidade,
+        uf: cliente.uf,
+        bairro: cliente.bairro,
+        numero: cliente.numero,
+        complemento: cliente.complemento ?? undefined,
+        telefones: cliente.telefones.map((t) => t.numero),
+      };
+    } catch (error) {
+      throw new Error(`Erro ao criar cliente: ${error.message}`);
+    }
   }
-}
-
 
   findAll() {
     return `This action returns all clients`;
@@ -83,5 +66,17 @@ export class ClientsService {
 
   remove(id: number) {
     return `This action removes a #${id} client`;
+  }
+
+  private _mapCreateClientDtoToPrismaData(dto: CreateClientDto) {
+    const { telefones, ...rest } = dto;
+    return {
+      ...rest,
+      telefones: {
+        createMany: {
+          data: telefones.map((numero) => ({ numero })),
+        },
+      },
+    };
   }
 }
