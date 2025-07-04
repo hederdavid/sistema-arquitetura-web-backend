f3c0287672f538b6b73cee79b3b18ac7065e00ec
hederdavid <hedermd6@gmail.com>
2025-07-04 14:28:23 -0300

    feat: config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule } from './clients/clients.module';
 import { DatabaseModule } from './plugins/database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [ClientsModule, DatabaseModule],
+  imports: [ClientsModule, DatabaseModule, ConfigModule, TasksModule],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const ssl = process.env.SSL === 'true';
   
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Servihub')
@@ -17,6 +20,15 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3001);
+  const config = app.get(ConfigService);
+
+  const HTTP_PORT = process.env.HTTP_PORT || '3000';
+  const APP_HOSTNAME = config.get<string>('APP_HOSTNAME');
+
+  await app.listen(HTTP_PORT, () => {
+    const address =
+      'http' + (ssl ? 's' : '') + '://' + APP_HOSTNAME + ':' + HTTP_PORT + '/';
+    Logger.log('Listening at ' + address);
+  });
 }
 bootstrap();
