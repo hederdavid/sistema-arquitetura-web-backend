18ebaca106112b33d5934daafe2927ce860b56a6
hederdavid <hedermd6@gmail.com>
2025-07-04 14:31:39 -0300

    fix: wrong import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { ClientsModule } from './clients/clients.module';
 import { DatabaseModule } from './plugins/database/database.module';
 import { ConfigModule } from '@nestjs/config';
-import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [ClientsModule, DatabaseModule, ConfigModule, TasksModule],
+  imports: [ClientsModule, DatabaseModule, ConfigModule],
   controllers: [],
   providers: [],
 })
